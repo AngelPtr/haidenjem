87d66f548ed7b009f2994466eb5455fe4dc27a69
AngelPtr <angelina2021@mhs.mdp.ac.id>
2024-05-30 00:56:51 +0700

    Detail Screen
    Mengisi detail screen tahap awal

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,16 +1,68 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
-class DetailScreen extends StatefulWidget {
-  const DetailScreen({super.key});
+class DetailScreen extends StatelessWidget {
+  final String documentId;
+  final String imageUrl;
+  final String title;
+  final String description;
+  final Timestamp timestamp;
+  final String userEmail;
 
-  @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
+  const DetailScreen({
+    required this.documentId,
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+    required this.timestamp,
+    required this.userEmail,
+  });
 
-class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              height: 200,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'judul: $title',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Deskripsi: $description',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Timestamp: ${DateFormat.yMMMd().add_jm().format(timestamp.toDate())}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Posted by: $userEmail',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
