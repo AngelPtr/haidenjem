959fd155ed9bd3f2a857386827697f5e874d2d21
SrFebry <febryanmdp@mhs.mdp.ac.id>
2024-05-30 00:11:19 +0700

    Home Screen
    pembuatan tampilan home screen untuk menampilkan data yang sudah di post, lalu pembuatan navigation bar serta pembuatan class pada search, detail, profil, dan favorit

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({super.key});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:haidenjem/main.dart';
+import 'package:haidenjem/screens/detail_screen.dart';
+import 'package:haidenjem/screens/favorite_screen.dart';
+import 'package:haidenjem/screens/post_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:haidenjem/screens/post_screen.dart';
+import 'package:haidenjem/screens/profile_screen.dart';
+import 'package:haidenjem/screens/search_screen.dart';
 import 'package:haidenjem/screens/sign_in_screen.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late Stream<QuerySnapshot> _postsStream = Stream.empty();
+
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    HomeScreen(),
+    SearchScreen(),
+    PostScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+
   Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
@@ -12,10 +39,19 @@ class HomeScreen extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _postsStream = FirebaseFirestore.instance
+        .collection('Post')
+        .orderBy('timestamp')
+        .snapshots();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Text('HaidenJem'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
           IconButton(
@@ -24,21 +60,163 @@ class HomeScreen extends StatelessWidget {
             },
             icon: const Icon(Icons.logout),
           ),
+          IconButton(
+            icon: Stack(
+              children: [
+                Icon(Icons.notifications),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 12,
+                      minHeight: 12,
+                    ),
+                    child: Text(
+                      '',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            onPressed: () {
+              // Handle the notification button press
+            },
+          )
         ],
       ),
-      body: const Center(
-        child: Text('You have logged In'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => PostScreen()));
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _postsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+
+          return GridView.builder(
+            padding: const EdgeInsets.all(8.0),
+            itemCount: snapshot.data!.docs.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.7,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemBuilder: (context, index) {
+              final DocumentSnapshot documentSnapshot =
+                  snapshot.data!.docs[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(
+                        documentId: documentSnapshot.id,
+                        imageUrl: documentSnapshot['url'],
+                        title: documentSnapshot['judul'],
+                        description: documentSnapshot['deskripsi'],
+                        timestamp: documentSnapshot['timestamp'],
+                        userEmail: documentSnapshot['user_email'],
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(documentSnapshot['url']),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
         },
-        tooltip: 'Add Post',
-        child: const Icon(
-          Icons.add,
-          color: Colors.blueAccent,
-        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => _screens[index]),
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Icon(Icons.home),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Icon(Icons.search),
+            ),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Icon(Icons.add),
+            ),
+            label: 'Post',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Icon(Icons.favorite),
+            ),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Icon(Icons.person),
+            ),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
