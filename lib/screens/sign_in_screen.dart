d9f53207d92ca144c8df617c911d26bd8f8ae4dd
MichelleGrc <michellegraciela@mhs.mdp.ac.id>
2024-05-29 23:29:35 +0700

    Otentikasi
    Sign in dan sign out

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -9,7 +9,7 @@ final googleSignIn = GoogleSignIn(
     'email',
   ],
   clientId:
-      '231154984703-6jm1g98dk79o0a6a3s5uoauapr19q63e.apps.googleusercontent.com',
+  '231154984703-6jm1g98dk79o0a6a3s5uoauapr19q63e.apps.googleusercontent.com',
 );
 
 class SignInScreen extends StatefulWidget {
@@ -29,7 +29,7 @@ class SignInScreenState extends State<SignInScreen> {
       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
       if (googleUser != null) {
         final GoogleSignInAuthentication googleAuth =
-            await googleUser.authentication;
+        await googleUser.authentication;
         final credential = GoogleAuthProvider.credential(
           accessToken: googleAuth.accessToken,
           idToken: googleAuth.idToken,
@@ -53,91 +53,107 @@ class SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign In'),
+        toolbarHeight: 350,
+        title: Image.asset(
+          'assets/images/logo.png',
+          height: 100,
+          width: 500,
+          fit: BoxFit.contain,
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const SizedBox(height: 32.0),
+              const SizedBox(height: 20.0),
               TextField(
                 controller: _emailController,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   labelText: 'Email',
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 9, style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
                 ),
               ),
               const SizedBox(height: 16.0),
               TextField(
                 controller: _passwordController,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   labelText: 'Password',
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 9, style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
                 ),
                 obscureText: true,
               ),
               const SizedBox(height: 16.0),
-              ElevatedButton.icon(
-                  onPressed: _signInWIthGoogle,
-                  icon: Image.asset(
-                    'assets/images/google.png',
-                    width: 20,
-                    height: 20,
-                  ),
-                  label: const Text(
-                    'Sign In with Google',
-                  )),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () async {
-                  final email = _emailController.text.trim();
-                  final password = _passwordController.text;
-                  // Validasi email
-                  if (email.isEmpty || !isValidEmail(email)) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text('Please enter a valid email')),
-                    );
-                    return;
-                  }
-                  // Validasi password
-                  if (password.isEmpty) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text('Please enter your password')),
-                    );
-                    return;
-                  }
-                  try {
-                    // Lakukan sign in dengan email dan password
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                    // Jika berhasil sign in, navigasi ke halaman beranda
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                          builder: (context) => const HomeScreen()),
-                    );
-                  } on FirebaseAuthException catch (error) {
-                    print('Error code: ${error.code}');
-                    if (error.code == 'user-not-found') {
-                      // Jika email tidak terdaftar, tampilkan pesan kesalahan
+              Container(
+                height: 40,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final email = _emailController.text.trim();
+                    final password = _passwordController.text;
+                    // Validasi email
+                    if (email.isEmpty || !isValidEmail(email)) {
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
-                            content: Text('No user found with that email')),
+                            content: Text('Please enter a valid email')),
                       );
-                    } else if (error.code == 'wrong-password') {
-                      // Jika password salah, tampilkan pesan kesalahan
+                      return;
+                    }
+                    // Validasi password
+                    if (password.isEmpty) {
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
-                            content: Text('Wrong password. Please try again.')),
+                            content: Text('Please enter your password')),
                       );
-                    } else {
-                      // Jika terjadi kesalahan lain, tampilkan pesan kesalahan umum
+                      return;
+                    }
+                    try {
+                      // Lakukan sign in dengan email dan password
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      // Jika berhasil sign in, navigasi ke halaman beranda
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                      );
+                    } on FirebaseAuthException catch (error) {
+                      print('Error code: ${error.code}');
+                      if (error.code == 'user-not-found') {
+                        // Jika email tidak terdaftar, tampilkan pesan kesalahan
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text('No user found with that email')),
+                        );
+                      } else if (error.code == 'wrong-password') {
+                        // Jika password salah, tampilkan pesan kesalahan
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content:
+                              Text('Wrong password. Please try again.')),
+                        );
+                      } else {
+                        // Jika terjadi kesalahan lain, tampilkan pesan kesalahan umum
+                        setState(() {
+                          _errorMessage = error.message ?? 'An error occurred';
+                        });
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(_errorMessage),
+                          ),
+                        );
+                      }
+                    } catch (error) {
+                      // Tangani kesalahan lain yang tidak terkait dengan otentikasi
                       setState(() {
-                        _errorMessage = error.message ?? 'An error occurred';
+                        _errorMessage = error.toString();
                       });
                       ScaffoldMessenger.of(context).showSnackBar(
                         SnackBar(
@@ -145,21 +161,53 @@ class SignInScreenState extends State<SignInScreen> {
                         ),
                       );
                     }
-                  } catch (error) {
-                    // Tangani kesalahan lain yang tidak terkait dengan otentikasi
-                    setState(() {
-                      _errorMessage = error.toString();
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(_errorMessage),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Sign In'),
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
               ),
-              const SizedBox(height: 32.0),
+              const SizedBox(height: 16.0),
+              Container(
+                height: 45,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _signInWIthGoogle,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/google.png',
+                        width: 20,
+                        height: 20,
+                      ),
+                      const SizedBox(width: 16.0),
+                      const Text(
+                        'Sign In with Google',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               TextButton(
                 onPressed: () {
                   Navigator.push(
@@ -168,7 +216,26 @@ class SignInScreenState extends State<SignInScreen> {
                         builder: (context) => const SignUpScreen()),
                   );
                 },
-                child: const Text('Don\'t have an account? Sign up'),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Doesn\'t have an account? ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Register here!',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:haidenjem/screens/home_screen.dart';
+import 'package:haidenjem/screens/sign_in_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -15,75 +16,157 @@ class SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign Up'),
+        title: const Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 32.0),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 300),
+              const Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+              const SizedBox(height: 50.0),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                        50), // add a border radius to make the border rounded
+                    borderSide: BorderSide(
+                        width: 2,
+                        color:
+                        Colors.black), // change the border width and color
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () async {
-                final email = _emailController.text.trim();
-                final password = _passwordController.text.trim();
-                if (email.isEmpty || password.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please fill in all fields.')),
-                  );
-                  return;
-                }
-                try {
-                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => const HomeScreen()),
-                  );
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'weak-password') {
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                        50), // add a border radius to make the border rounded
+                    borderSide: BorderSide(
+                        width: 2,
+                        color:
+                        Colors.black), // change the border width and color
+                  ),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () async {
+                  final email = _emailController.text.trim();
+                  final password = _passwordController.text.trim();
+                  if (email.isEmpty || password.isEmpty) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
-                          content: Text('The password provided is too weak.')),
+                          content: Text('Please fill in all fields.')),
                     );
-                  } else if (e.code == 'email-already-in-use') {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text(
-                              'The account already exists for that email.')),
+                    return;
+                  }
+                  try {
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
                     );
-                  } else if (e.code == 'invalid-email') {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content:
+                            Text('The password provided is too weak.')),
+                      );
+                    } else if (e.code == 'email-already-in-use') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text(
+                                'The account already exists for that email.')),
+                      );
+                    } else if (e.code == 'invalid-email') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('The email address is not valid.')),
+                      );
+                    }
+                  } catch (e) {
                     ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text('The email address is not valid.')),
+                      SnackBar(content: Text('An error occurred: $e')),
                     );
                   }
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('An error occurred: $e')),
+                },
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SignInScreen()),
                   );
-                }
-              },
-              child: const Text('Sign Up'),
-            ),
-          ],
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Already have an account? ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Login Here!',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
