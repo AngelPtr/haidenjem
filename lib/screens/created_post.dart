139f0b65d3e81036e5600f885e4779dae6fd430c
ChintyaLyra <chintyalyra@mhs.mdp.ac.id>
2024-06-21 15:43:15 +0700

    Created Post
    Tambah halaman created post pengguna

---
+++ lib/screens/created_post.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:haidenjem/screens/detail_screen.dart';
+import 'package:haidenjem/screens/edit_post.dart';
+
+class CreatedPostScreen extends StatefulWidget {
+  @override
+  _CreatedPostScreenState createState() => _CreatedPostScreenState();
+}
+
+class _CreatedPostScreenState extends State<CreatedPostScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Created Posts'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _firestore
+            .collection('Post')
+            .where('user_email', isEqualTo: _auth.currentUser!.email)
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot document = snapshot.data!.docs[index];
+                final imageUrl = document['url'];
+                final title = document['judul'];
+                final description = document['deskripsi'];
+
+                return Card(
+                  child: ListTile(
+                    title: Text(title),
+                    subtitle: Text(description),
+                    leading: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Image.network(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditPostScreen(
+                              documentId: document.id,
+                              imageUrl: imageUrl,
+                              title: title,
+                              description: description,
+                              timestamp: document['timestamp'],
+                              userEmail: document['user_email'],
+                              latitude: document['latitude'],
+                              longitude: document['longitude'],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                            documentId: document.id,
+                            imageUrl: imageUrl,
+                            title: title,
+                            description: description,
+                            timestamp: document['timestamp'],
+                            userEmail: document['user_email'],
+                            latitude: document['latitude'],
+                            longitude: document['longitude'],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
