8b9c91650702b4b4825cf40f7cfa85b9ee4d3324
AngelPtr <angelina2021@mhs.mdp.ac.id>
2024-06-21 15:51:42 +0700

    Perbaruan main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,55 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:haidenjem/firebase_options.dart';
 import 'package:haidenjem/screens/home_screen.dart';
 import 'package:haidenjem/screens/sign_in_screen.dart';
+import 'package:haidenjem/service/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  final themeProvider = ThemeProvider();
+  await themeProvider.initDarkMode();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => themeProvider,
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Fasum',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          } else {
-            return const SignInScreen();
-          }
-        },
-      ),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'HaidenJem Apps',
+          theme: themeProvider.isDarkMode
+              ? ThemeData(
+            brightness: Brightness.dark,
+            appBarTheme: AppBarTheme(),
+          )
+              : ThemeData(
+            brightness: Brightness.light,
+            appBarTheme: AppBarTheme(),
+          ),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                //return NavBarScreen();
+                return const HomeScreen();
+              } else {
+                return const SignInScreen();
+              }
+            },
+          ),
+        );
+      },
     );
   }
 }
